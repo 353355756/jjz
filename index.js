0783e906118c71448d8ae5447abd62754bc1f457
liuliang <353355756@qq.com>
2017-12-11 14:07:20 +0800

    xiugai

--- index.js
+++ index.js
@@ -1,34 +1,34 @@
 const rp = require('request');
-const domin = 'https://api.jinjingzheng.zhongchebaolian.com';
+const domin = 'https://enterbj.zhongchebaolian.com';
 const addcartypeUrl = '/enterbj/platform/enterbj/addcartype';
 
 const addcartype = {
-  uri: domin+addcartypeUrl,
-  method: 'GET',
+  url: domin + addcartypeUrl,
+  method: 'POST',
   gzip: true,
   headers: {
     'Content-Type': 'application/x-www-form-urlencoded',
-    'Origin': 'https://api.jinjingzheng.zhongchebaolian.com',
-    'Cookie': 'JSESSIONID=03451CCEFCA2975A892618509F49EB2A; CNZZDATA1260761932=1447194544-1489704663-https%253A%252F%252Fapi.jinjingzheng.zhongchebaolian.com%252F%7C1494198185; UM_distinctid=15ab66a50aa19-0ebf95ce9e49a4-2e5e056a-3d10d-15ab66a50ab7c',
-    'Content-Length': 182,
-    'Connection': 'keep-alive',
+    //'Origin': 'https://api.jinjingzheng.zhongchebaolian.com',
+    'Cookie': 'JSESSIONID=7D57B56813185A501A9E55852EEA211A; UM_distinctid=160414f70260-03d489db4c86e1-4b065c28-49a10-160414f702955; CNZZDATA1260761932=296000769-1512920721-%7C1512920721',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
     'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_2 like Mac OS X) AppleWebKit/603.2.4 (KHTML, like Gecko) Mobile/14F89',
-    'Referer': 'https://api.jinjingzheng.zhongchebaolian.com/enterbj/jsp/enterbj/index.jsp',
+   // 'Referer': 'https://api.jinjingzheng.zhongchebaolian.com/enterbj/jsp/enterbj/index.jsp',
     'Accept-Language': 'zh-cn',
     'Accept-Encoding': 'gzip,deflate'
   },
   form: {
     applyid: '',
-    carid: '10223597',
-    userid: '36ACEF1E65A842F3B9520936D70178FE',
-    gpslon: '116.314587',
-    gpslat: '40.044848',
-    imei: '',
-    imsi: '',
-    licenseno: '%E5%86%80F362SF',
-    appsource: '',
-    hiddentime: '2017-12-09+00%3A46%3A52'
+    carid: '19201211',
+    userid: '0EA7FE7CC30B4DB7B0FF4A814434A296',
+    gpslon: '4.9E-324',
+    gpslat: '4.9E-324',
+    imei: '630d99f5-ceb7-3f39-97bd-1391bb918f8d',
+    imsi: '460018341611869',
+    licenseno: '苏HB9520',
+    appsource: 'bjjj',
+    hiddentime: '2017-12-11+01:15:34',
+    cartype: '02',
+    vehicletype: '03'
   },
   followRedirect: false
 };
@@ -37,12 +37,12 @@ const hcode = 302;
 
 
 console.time('addcartype');
-const si = setInterval(function(){ rp(addcartype, callback); }, 1000);
+const si = setInterval(function(){ console.log(1); rp.post(addcartype, callback); }, 1000);
 
 let i = 0;
 function callback(error, response, body) {
   console.log('截止到%d,请求次数%d' , new Date(), i++);
-  console.log(body);
+  console.log('&&&&&&&&&&&&&&&',body);
   if (!error && response.statusCode == 200) {
     clearInterval(si);
     console.timeEnd('addcartype');
